e3f8ef9ace4e487839550a50526d3afb2ec257c4
Julie Zelenski <zelenski@cs.stanford.edu>
2021-02-17 06:26:47 +0000

    Also update libref pi commit 954e9e6046a68ac023f7282eaa352ef4ac7887ee Author: Julie Zelenski <zelenski@cs.stanford.edu> Date:   Tue Feb 16 22:19:47 2021 -0800
        Also update libref pi

--- cs107e/include/ref/gpio_interrupts_ref.h
+++ cs107e/include/ref/gpio_interrupts_ref.h
@@ -4,4 +4,4 @@ void ref_gpio_interrupts_init(void);
 void ref_gpio_interrupts_enable(void);
 void ref_gpio_interrupts_disable(void);
 handler_fn_t ref_gpio_interrupts_register_handler(unsigned int pin, handler_fn_t fn, void *aux_data);
-bool ref_gpio_default_handler(unsigned int pc, void *aux_data);
+void ref_gpio_default_handler(unsigned int pc, void *aux_data);
