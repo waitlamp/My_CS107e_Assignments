b7fd95a6784ec77a13fa7ced9298d7ada8d8f616
Julie Zelenski <zelenski@cs.stanford.edu>
2021-02-21 08:56:47 +0000

    console_init takes fg/bg color commit fd333572722209b831c7034bb88f68a0e28b873f Author: Julie Zelenski <zelenski@cs.stanford.edu> Date:   Sun Feb 21 00:50:53 2021 -0800
        console_init takes fg/bg color

--- cs107e/include/ref/shell_commands_ref.h
+++ cs107e/include/ref/shell_commands_ref.h
@@ -2,6 +2,6 @@
 #include "shell_commands.h"
 int ref_cmd_echo(int argc, const char *argv[]);
 int ref_cmd_help(int argc, const char *argv[]);
-int ref_cmd_reboot(int argc, const char* argv[]);
-int ref_cmd_peek(int argc, const char* argv[]);
-int ref_cmd_poke(int argc, const char* argv[]);
+int ref_cmd_reboot(int argc, const char *argv[]);
+int ref_cmd_peek(int argc, const char *argv[]);
+int ref_cmd_poke(int argc, const char *argv[]);
