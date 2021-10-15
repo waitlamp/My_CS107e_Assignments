a9a65658afdb6773d90b8236625922c6a2f91208
Chris Gregg <tofergregg@gmail.com>
2021-10-15 16:54:39 +0000

    Merge branch 'master' of github.com:cs107e/staff commit c1c9a093f0b4fcbbcb247010978f53bb8193c8e3 Author: Chris Gregg <tofergregg@gmail.com> Date:   Fri Oct 15 09:49:42 2021 -0700
        Merge branch 'master' of github.com:cs107e/staff

--- lectures/C_Pointers/code/button/c_button.c
+++ lectures/C_Pointers/code/button/c_button.c
@@ -1,7 +1,7 @@
-static unsigned int *FSEL1 =  (unsigned int *)0x20200004;
-static unsigned int *FSEL2 =  (unsigned int *)0x20200008;
-static unsigned int *SET0  =  (unsigned int *)0x2020001c;
-static unsigned int *LEV0  =  (unsigned int *)0x20200034;
+volatile static unsigned int *FSEL1 =  (unsigned int *)0x20200004;
+volatile static unsigned int *FSEL2 =  (unsigned int *)0x20200008;
+volatile static unsigned int *SET0  =  (unsigned int *)0x2020001c;
+volatile static unsigned int *LEV0  =  (unsigned int *)0x20200034;
 
 void main(void)
 {
